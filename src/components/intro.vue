a808ee3dbfa927f482c21aa0d602673dc4f0a509
sofyan48 <meongbego@gmail.com>
2025-01-12 08:59:00 +0700

    Readme

--- src/components/intro.vue
+++ src/components/intro.vue
@@ -1,6 +1,6 @@
 <template>
     <div class="flex flex-col md:gap-y-8  md:py-60 py-4">
-        <h1 class="text-gray-600 poppins font-semibold text-5xl text-start">Hi, I’m <span class="font-bold text-gray-800">Iank</span>, a Software Architect</h1>
+        <h1 class="text-gray-600 poppins font-semibold text-5xl text-start">Hi, I’m <span class="font-bold text-gray-800">Iank</span>, a Software Engineer</h1>
         <div class="flex flex-col">
             <p class="text-base text-gray-500">Turning ideas into powerful solutions 🚀</p>
             <p class="text-base text-gray-500">Curious? Ask Ocha to find more information about me.!</p>
