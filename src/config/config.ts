a73bdc32575478cdba2032a7fb33098c93e22e5e
sofyan48 <meongbego@gmail.com>
2025-02-10 05:53:17 +0700

    Bearer support

--- src/config/config.ts
+++ src/config/config.ts
@@ -8,7 +8,7 @@ export const getconfig = (): IConfig => {
     const config: IConfig = {
         baseUrl : import.meta.env.APP_URL ?? "https://ocha.iank.me/ex",
         env     : import.meta.env.APP_MODE ?? "dev",
-        bearer: import.meta.env.BEARER ?? "xxxxx",
+        bearer: import.meta.env.BEARER ?? "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJhcGlrZXkiOiJvY2hhYm90Iiwicm9sZXMiOiJjbGllbnQiLCJleHAiOjE3NzA2NzY3ODB9.kmu2QhvpaikwWIl9ZpSvHJqmGTYsvAFUBDssOZmab7k",
     }
 
     return config
